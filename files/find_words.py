b0f2f1cc94fe5df6d33695f2fa5743884ba715c7
NickSheva <nikolayshevtsov@mail.ru>
2023-06-09 16:43:10 +0500

    find list of words of length more or equals 4 letters

---
+++ files/find_words.py
@@ -0,0 +1,17 @@
+"""Find words more or equalse four letters"""
+
+## Данные
+text = """Call me Ishmael. Some years ago - never mind how long precisely - having
+little or no money in my purse, and nothing particular to interest me
+on shore, I thought I would sail about a little and see the watery part
+of the world. It is a way I have of driving off the spleen, and regulating
+the circulation. - Moby Dick"""
+
+
+def find_len():
+    word = [[x for x in line.split() if len(x) >= 4] for line in text.split("\n")]
+    print(word)
+
+
+if __name__ == "__main__":
+    find_len()
