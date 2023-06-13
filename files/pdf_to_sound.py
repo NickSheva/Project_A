eba7fc0c835b1d365b72cca20ab696d10488a9d3
NickSheva <nikolayshevtsov@mail.ru>
2023-06-13 15:55:44 +0500

    share the text for pages with extract_text in plumber.PDF

--- files/pdf_to_sound.py
+++ files/pdf_to_sound.py
@@ -8,10 +8,9 @@ from pathlib import Path
 def pdf_to_mp3(path='file.pdf', lang='ru'):
     if Path(path).is_file() and Path(path).suffix == '.pdf':
         with pdfplumber.PDF(open(file=path, mode='rb')) as pdf:
-            pages = [page.extract.text() for page in pdf.pages]
+            pages = [page.extract_text() for page in pdf.pages]
+            return pages
 
-
-        return "Yes"
     else:
         return 'File does not exit'
 
