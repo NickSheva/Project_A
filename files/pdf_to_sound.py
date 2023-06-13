b3b5fa7c133f4c9f5eb3286e126ad21b1be3668b
NickSheva <nikolayshevtsov@mail.ru>
2023-06-13 15:41:46 +0500

    change def's name, download the pdf file, check there is a file with suffix .pdf

--- files/pdf_to_sound.py
+++ files/pdf_to_sound.py
@@ -5,8 +5,16 @@ from gtts import gTTS
 from pathlib import Path
 
 
-def main(path='file.pdf', lang='ru'):
-    pass
+def pdf_to_mp3(path='file.pdf', lang='ru'):
+    if Path(path).is_file() and Path(path).suffix == '.pdf':
+        return "Yes"
+    else:
+        return 'File does not exit'
+
+
+def main():
+    path = input("\nEnter the file's path:\n")
+    print(pdf_to_mp3(path=path))
 
 
 if __name__ == '__main__':
