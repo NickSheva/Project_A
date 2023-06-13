df977f825b10b5e62f86f90d1edd48d0487e58eb
NickSheva <nikolayshevtsov@mail.ru>
2023-06-13 15:48:16 +0500

    share the text for pages

--- files/pdf_to_sound.py
+++ files/pdf_to_sound.py
@@ -7,6 +7,10 @@ from pathlib import Path
 
 def pdf_to_mp3(path='file.pdf', lang='ru'):
     if Path(path).is_file() and Path(path).suffix == '.pdf':
+        with pdfplumber.PDF(open(file=path, mode='rb')) as pdf:
+            pages = [page.extract.text() for page in pdf.pages]
+
+
         return "Yes"
     else:
         return 'File does not exit'
