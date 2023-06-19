7bebc0a06bd615645bce5bc25f2d4a81f3f3e761
NickSheva <nikolayshevtsov@mail.ru>
2023-06-19 14:12:48 +0500

    added colors

--- files/draw_pie.py
+++ files/draw_pie.py
@@ -5,12 +5,15 @@ import matplotlib.pyplot as plt
 sales = [950000, 650000, 450000, 1600000, 60000, 40000,
          3500000, 100000, 60000, 500000]
 labels = ["Rolex", "Omega", "Cartier", "Longines", "Patek Philippe",
-          "Audemars Piguet", "Tissot", "IWC", "Hublot", "TAG Heuer", ]
+          "Audemars Piguet", "Tissot", "IWC", "Hublot", "TAG Heuer"]
 explode = [0.1, 0, 0, 0, 0.2, 0, 0, 0.1, 0, 0]
+colors = ["green", "cyan", "lightyellow", "red", "yellow", "pink",
+          "lightgray", "Lightblue", "lightgreen", "orange"]
 
 
 fig, ax = plt.subplots()
-ax.pie(sales, labels=labels, explode=explode, shadow=True, startangle=150, autopct="%1.1f%%")
+ax.pie(sales, labels=labels, explode=explode, colors=colors, shadow=True,
+       startangle=150, autopct="%1.1f%%")
 ax.axis("equal")
 
 plt.show()
