acf86140268b4b4acae6906eb1e6147da3980b2a
NickSheva <nikolayshevtsov@mail.ru>
2023-06-19 13:22:07 +0500

    added  axis("equal") and more explode for reading

--- files/draw_pie.py
+++ files/draw_pie.py
@@ -6,11 +6,11 @@ sales = [950000, 650000, 450000, 1600000, 60000, 40000,
          3500000, 100000, 60000, 500000]
 labels = ["Rolex", "Omega", "Cartier", "Longines", "Patek Philippe",
           "Audemars Piguet", "Tissot", "IWC", "Hublot", "TAG Heuer", ]
-explode = [0.1, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+explode = [0.1, 0, 0, 0, 0.2, 0, 0, 0.1, 0, 0]
 
 
 fig, ax = plt.subplots()
 ax.pie(sales, labels=labels, explode=explode, shadow=True, startangle=150)
-
+ax.axis("equal")
 
 plt.show()
